da4eca0831f113f368a7d3a37288cb7e7683200f
Natnael Yeshiwas <natiyeshi16@gmail.com>
2025-04-23 14:17:19 +0300

    ASDF

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,9 @@
 import { ICBlog } from "@/interfaces/blog.interface";
 import { MetadataRoute } from "next";
 
-const fetchBlogs = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/api/blog`
-        );
-        const data: ICBlog[] = await response.json();
-        return data;
-      } catch (error) {
-        console.error("Error fetching blogs:", error);
-      } finally {
-        return []
-      }
-};
 
 export default async function sitemap() : Promise<MetadataRoute.Sitemap>{
-  const URL = "https://gaber-wear.org"
-  const blogs  = await fetchBlogs();
-  const blogsSitemap : MetadataRoute.Sitemap = blogs ? blogs.map(blog => ({
-    url : `${URL}/blog/${blog.link}`,
-  })) : []
-
+  const URL = "https://chemodos.com"
     return [
         {
             url : `${URL}`,
@@ -30,17 +12,14 @@ export default async function sitemap() : Promise<MetadataRoute.Sitemap>{
             url : `${URL}/products`,
         },
         {
-            url : `${URL}/ourstory`,
+            url : `${URL}/about`,
+        },
+        {
+            url : `${URL}/contact`,
         },
         {
             url : `${URL}/services`,
         },
-        {
-            url : `${URL}/contactus`,
-        },
-        {
-            url : `${URL}/blogs`,
-        },
-        ...blogsSitemap,
+        
     ]
 }
